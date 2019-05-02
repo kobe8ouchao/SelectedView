a1a6cae334da0dc016fd03d09bb99833c08aa38f
Ouchao <ouchao>
2019-05-02 23:20:48 +0800

    new

---
+++ SelectedView/SelectedView.h
@@ -0,0 +1,18 @@
+//
+//  SelectedView.h
+//  SelectedView
+//
+//  Created by liubei on 2019/5/2.
+//  Copyright © 2019 liubei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SelectedView : UIView
+@property (retain, nonatomic) UIScrollView *myScrollView;
+
+@end
+
+NS_ASSUME_NONNULL_END
